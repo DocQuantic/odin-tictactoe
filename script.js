d59182e1dcff88ab8e7bee2aa8d6538715cabd85
DocQuantic <35266773+DocQuantic@users.noreply.github.com>
2025-04-24 21:51:44 +0200

    Create GameManager, GameBoard and Player modules
    GameManager:
    Add function to increase turn value.
    
    GameBoard:
    Grid value are stored in an array.
    Add function to setup empty grid depending on a grid size value.
    Add function to set a specific grid coordinate to custom value.
    Add function to return grid.
    
    Player:
    Add function to increase score.
    Add function to return score.

--- script.js
+++ script.js
@@ -1,1 +1,46 @@
-console.log("Hello, World!")
+const gameBoard = (function(){
+    let gridSize = 3;
+    let grid;
+    
+    function setupGrid(){
+        grid = new Array(gridSize**2);
+        for(let i=0; i<grid.length; i++){
+            grid[i] = "";
+        }
+    }
+
+    function setGridElement(row, col, value){
+        grid[(col-1)+gridSize*(row-1)] = value;
+    }
+
+    function getGrid(){
+        return grid;
+    }
+
+    setupGrid();
+    
+    return {setGridElement, getGrid};
+})();
+
+const gameManager = (function(){
+    let turnNumber = 0;
+    let currentPlayer = 1;
+
+    function nextTurn(){
+        turnNumber++;
+    }
+})();
+
+function createPlayer(name){
+    let score = 0;
+
+    function increaseScore(){
+        score++;
+    }
+
+    function getScore(){
+        return score;
+    }
+
+    return {name, increaseScore, getScore};
+}
