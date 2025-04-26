0c54ddc5aac8ffa0c2bbb66dee30a5c678dca6e2
DocQuantic <35266773+DocQuantic@users.noreply.github.com>
2025-04-26 21:27:52 +0200

    Working console version
    After each turn, the game checks winning conditions, declares winner if
    any, increase score of the winner and resets the game.

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
-const gameBoard = (function(){
-    let gridSize = 3;
+function createGameBoard (gridSize) {
     let grid;
     
     function setupGrid(){
         grid = new Array(gridSize**2);
         for(let i=0; i<grid.length; i++){
-            grid[i] = "";
+            grid[i] = 0;
         }
     }
 
@@ -17,30 +16,109 @@ const gameBoard = (function(){
         return grid;
     }
 
+    function showGrid(){
+        console.log(grid);
+    }
+
+    function resetGrid(){
+        for(let i=0; i<grid.length; i++){
+            grid[i] = 0;
+        }
+    }
+
     setupGrid();
     
-    return {setGridElement, getGrid};
-})();
+    return {setGridElement, getGrid, showGrid, resetGrid};
+};
 
 const gameManager = (function(){
     let turnNumber = 0;
-    let currentPlayer = 1;
+    const players = {player1: createPlayer("Player1", 1), player2: createPlayer("Player2", 2)}
+    let currentPlayer = players.player1;
+    const gridSize = 3;
+    const gameBoard = createGameBoard(gridSize);
+
+    function playTurn(row, col){
+        value = currentPlayer.getToken();
+
+        gameBoard.setGridElement(row, col, value);
+        gameBoard.showGrid();
+
+        const winner = checkEndGame();
+        const winPlayer = winner === 1 ? players.player1 : players.player2
+        if(winner !== 0){
+            console.log(`Congratulations ! ${winPlayer.name} won the game`);
+            winPlayer.increaseScore();
+
+            newGame();
+            return;
+        };
+
+        nextTurn();
+    }
+
+    function newGame(){
+        currentPlayer = players.player1;
+        turnNumber = 0;
+        gameBoard.resetGrid();
+    }
 
     function nextTurn(){
         turnNumber++;
+
+        currentPlayer = currentPlayer === players.player1 ? players.player2 : players.player1;
     }
+
+    function checkEndGame(){
+        grid = gameBoard.getGrid();
+        const isEqual = (element, i, arr) => element === arr[0];
+
+        //check rows and columns
+        for(let i=0; i<gridSize; i++){
+            const row = [grid[0 + gridSize*i], grid[1 + gridSize*i], grid[2 + gridSize*i]];
+            const col = [grid[i], grid[gridSize + i], grid[2*gridSize + i]]
+
+            if(row.every(isEqual) || col.every(isEqual)){
+                const winner = row.every(isEqual) ? row[0] : col[0];
+
+                return winner;
+            }
+        }
+
+        //check diagonals
+        const diag1 = [];
+        const diag2 = [];
+        for(let i=0; i<gridSize; i++){
+            diag1.push(grid[(gridSize+1)*i])
+            diag2.push(grid[(gridSize-1)*(i+1)])
+        }
+        
+        if(diag1.every(isEqual) || diag2.every(isEqual)){
+            const winner = diag1.every(isEqual) ? diag1[0] : diag2[0];
+
+            return winner;
+        }
+
+        return 0;
+    }
+
+    function createPlayer(name, token){
+        let score = 0;
+    
+        function increaseScore(){
+            score++;
+        }
+    
+        function getScore(){
+            return score;
+        }
+
+        function getToken(){
+            return token;
+        }
+    
+        return {name, increaseScore, getScore, getToken};
+    }
+
+    return {playTurn};
 })();
-
-function createPlayer(name){
-    let score = 0;
-
-    function increaseScore(){
-        score++;
-    }
-
-    function getScore(){
-        return score;
-    }
-
-    return {name, increaseScore, getScore};
-}
